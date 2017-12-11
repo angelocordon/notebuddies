ca2ba0602c5af8188a2328c7e5091fae396e24d4
Angelo Cordon <angelocordon@gmail.com>
2017-12-11 09:59:24 -0800

    Set video path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'videos#index'
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show], path: '/v/'
 end
